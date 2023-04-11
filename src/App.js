5395385e85dd52ccd2df3debe469c05acc49b686
Natete-Verite <nateteverite@gmail.com>
2023-04-11 15:39:10 +0200

    Electron with React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          Hello World!
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
+// hi supa
